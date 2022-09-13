dab3e89d46b6bc11d065932d1de8319713f55d3f
Divanchik-F <92337048+Divanchik-F@users.noreply.github.com>
2022-09-13 23:37:00 +0300

    Add files via upload

---
+++ Laba_2/absolute.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir /tmp/hello\ world/
+touch /tmp/hello\ world/absolute.txt
+echo "Hello world!" > /tmp/hello\ world/absolute.txt
+cat /tmp/hello\ world/absolute.txt
+ls -l /tmp/hello\ world/absolute.txt
