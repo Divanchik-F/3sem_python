061241d3294d913d05faa8454fee248892dc1967
Divanchik-F <92337048+Divanchik-F@users.noreply.github.com>
2022-11-07 13:38:47 +0300

    Update 9_3.py

--- Laba_9/9_3.py
+++ Laba_9/9_3.py
@@ -3,14 +3,14 @@ import string
 
 class TextLoader:
     def __init__(self, file_path):
-        self.path = file_path
-        self.files = os.listdir(self.path)
+        self.path=file_path
+        self.files=os.listdir(self.path)
 
     def __len__(self):
         return len(self.files)
 
     def __getitem__(self, i):
-        file_name = f"{self.path}\{self.files[i]}"
+        file_name=f"{self.path}\{self.files[i]}"
         with open(file_name, "r", encoding="utf-8") as file:
             return file.read().lower().translate(str.maketrans('', '', string.punctuation))
         
@@ -18,7 +18,7 @@ class TextLoader:
         for i in range(len(self.files)):
             yield self.__getitem__(i)
 
-txt_loader = TextLoader("D:\ILab\Akkinator\Lesson 9\sample")
+txt_loader=TextLoader("D:\ILab\Akkinator\Lesson 9\sample")
 print(len(txt_loader))
 print(txt_loader[0])
 for i in txt_loader:
